0cf7a40c2b595b013b09021cde6357fcd23a04c1
Kyosuke Takayama <loiseau@gmail.com>
2023-07-11 16:16:49 +0900

    initial import

---
+++ LocationBug/LocationBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationBugApp.swift
+//  LocationBug
+//
+//  Created by Kyosuke Takayama on 2023/07/11.
+//
+
+import SwiftUI
+
+@main
+struct LocationBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
